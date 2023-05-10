19d6e1ee03bef44c7111a1f696f399eb4a7e717d
Sabir Shaikh <44839291+therealsabirs@users.noreply.github.com>
2023-05-10 16:56:00 +0530

    Add files via upload

---
+++ Employee Details & Service Request~/InventoryV1/src/main/java/com/mycompany/inventoryv1/InventoryV1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inventoryv1;
+
+
+/**
+ *
+ * @author Sabir Shaikh
+ */
+
+
+
+public class InventoryV1 {
+
+    
+    
+    public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println("Hello World!");
+            Home ob = new Home();
+            ob.setVisible(true);
+            
+    }
+}
